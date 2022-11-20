3532ae6fb12374d4a35d8f325e49c978c9b76a83
dev-acc-practice <development-practice@proton.me>
2022-11-20 16:27:15 +0200

    Changed colors and fixed styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px'
+      'sm': "640px",
+      'md': "768px",
+      'lg': "1024px",
     },
     extend: {
       colors: {
-        "cyan": '#03DAC6',
-        "darkGray": '#292929',
-        "darkerGray": '#1F1F1F'
-      }
+        'brightGray': '#EBEBEB',
+        'almostAqua': '#CAD3C1',
+        'eerieBlack': '#1F1F1F'
+      },
     },
   },
   plugins: [],
-}
+};
